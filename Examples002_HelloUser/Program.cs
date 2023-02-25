eacb216783cbea5be043f6aab5f1dfa4126c4d54
Ksenia <stopisem@inbox.ru>
2023-02-25 22:45:16 +0500

    Sum

--- Examples002_HelloUser/Program.cs
+++ Examples002_HelloUser/Program.cs
@@ -1,3 +1,4 @@
-﻿string username = Console.ReadLine();
+﻿Console.Write("Введите Ваше имя");
+string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
---
+++ Examples003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int A = 66;
+int B = 75;
+int result = A + B;
+Console.WriteLine(result);
