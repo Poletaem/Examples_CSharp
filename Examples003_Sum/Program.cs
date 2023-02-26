fee52ba83178e84be51b3d3daf223eb7bedcbd73
Ksenia <stopisem@inbox.ru>
2023-02-26 09:17:29 +0500

    Random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int A = 66;
-int B = 75;
+﻿int A = new Random().Next(1,10);
+Console.WriteLine(A);
+int B = new Random().Next(1,10);
+Console.WriteLine(B);
 int result = A + B;
 Console.WriteLine(result);
